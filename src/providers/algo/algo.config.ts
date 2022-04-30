b5e5b3705f53af07751c8bf4d36da4ca0caca440
Eroo36 <edogruca@hotmail.com>
2022-04-30 14:57:19 +0300

    added send algos

--- src/providers/algo/algo.config.ts
+++ src/providers/algo/algo.config.ts
@@ -13,10 +13,17 @@ export enum Network {
   MAINNET = 'mainnet',
 }
 
+export type CreateAssetTxn = {
+  'asset-index': number;
+  'confirmed-round': number;
+  'pool-error': string;
+  txn: any;
+};
+
 export const TESTNET_ALGO_CONF: AlgoConfig = {
   apiKeyHeader: { 'x-api-key': process.env.ALGO_API_KEY },
-  indexerUri: 'https://mainnet-algorand.api.purestake.io/idx2',
-  clientUri: 'https://mainnet-algorand.api.purestake.io/ps2',
+  indexerUri: 'https://testnet-algorand.api.purestake.io/idx2',
+  clientUri: 'https://testnet-algorand.api.purestake.io/ps2',
   addr: process.env.TESTNET_ALGO_ACCOUNT,
   sk: process.env.TESTNET_ALGO_SECRET,
 };
--- src/providers/algo/algo.service.ts
+++ src/providers/algo/algo.service.ts
@@ -1,7 +1,8 @@
 import { Logger } from '@nestjs/common';
-import algosdk, { Algodv2, Indexer, Transaction } from 'algosdk';
+import algosdk, { Account, Algodv2, Indexer, Transaction } from 'algosdk';
 import {
   AlgoConfig,
+  CreateAssetTxn,
   MAINNET_ALGO_CONF,
   Network,
   TESTNET_ALGO_CONF,
@@ -38,47 +39,48 @@ export class AlgoService {
     unitName: string;
     assetName: string;
     metadataUrl: string;
+    from: {
+      addr: string;
+      sk: string;
+    };
   }) {
-    const { unitName, assetName, metadataUrl } = params;
+    const { unitName, assetName, metadataUrl, from } = params;
     const txparams = await this.client.getTransactionParams().do();
     const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
-      from: process.env.ALGO_ACCOUNT,
+      from: from.addr,
       total: 1,
       decimals: 0,
       assetName,
       unitName,
       assetURL: metadataUrl,
       defaultFrozen: false,
-      // freeze: freezeAddr,
-      // manager: managerAddr,
-      // clawback: clawbackAddr,
-      // reserve: reserveAddr,
       suggestedParams: txparams,
     });
-    const completedTxn = await this.signAndSendTxn(txn);
+    const completedTxn = (await this.signAndSendTxn(
+      txn,
+      from.sk,
+    )) as CreateAssetTxn;
     return completedTxn;
   }
 
-  public async sendAsset(params: { to: string; assetId: number }) {
-    const { to, assetId } = params;
-    const suggestedParams = await this.client.getTransactionParams().do();
+  // public async sendAsset(params: { to: string; assetId: number }) {
+  //   const { to, assetId } = params;
+  //   const suggestedParams = await this.client.getTransactionParams().do();
 
-    const txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
-      amount: 1,
-      from: process.env.ALGO_ACCOUNT,
-      to,
-      suggestedParams,
-      assetIndex: assetId,
-    });
+  //   const txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+  //     amount: 1,
+  //     from: this.config.addr,
+  //     to,
+  //     suggestedParams,
+  //     assetIndex: assetId,
+  //   });
 
-    const completedTxn = await this.signAndSendTxn(txn);
-    return completedTxn;
-  }
+  //   const completedTxn = await this.signAndSendTxn(txn, this.config.sk);
+  //   return completedTxn;
+  // }
 
-  private async signAndSendTxn(txn: Transaction) {
-    const signedTxn = txn.signTxn(
-      algosdk.mnemonicToSecretKey(process.env.ALGO_SECRET).sk,
-    );
+  private async signAndSendTxn(txn: Transaction, sk: string) {
+    const signedTxn = txn.signTxn(algosdk.mnemonicToSecretKey(sk).sk);
     let txId = txn.txID().toString();
 
     await this.client.sendRawTransaction(signedTxn).do();
@@ -89,15 +91,46 @@ export class AlgoService {
       4,
     );
     Logger.log(`Transaction ${txId} confirmed`);
-    Logger.debug(confirmedTxn);
-    Logger.log('Transaction Fee: %d microAlgos', confirmedTxn.txn.txn.fee);
-    await this.getAccountInfo(process.env.ALGO_ACCOUNT);
+    Logger.log(`Transaction Fee: microAlgos: ${confirmedTxn.fee}`);
+    await this.getAccountInfo(this.config.addr);
     return confirmedTxn;
   }
 
   private async getAccountInfo(addr: string) {
     const accountInfo = await this.client.accountInformation(addr).do();
-    Logger.log('Account balance: %d microAlgos', accountInfo.amount);
+    Logger.log(`Account balance: ${accountInfo.amount} microAlgos`);
     return accountInfo;
+  }
+
+  // public async optInAsset(params: { account: Account; assetID: number }) {
+  //   const { account, assetID } = params;
+  //   const suggestedParams = await this.client.getTransactionParams().do();
+  //   const txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+  //     from: account.addr,
+  //     to: account.addr,
+  //     amount: 0,
+  //     assetIndex: assetID,
+  //     suggestedParams,
+  //   });
+  //   const completedTxn = await this.signAndSendTxn(
+  //     txn,
+  //     algosdk.secretKeyToMnemonic(account.sk),
+  //   );
+  //   return completedTxn;
+  // }
+
+  public async sendAlgos(params: { to: string; amount: number }) {
+    const { to, amount } = params;
+    const suggestedParams = await this.client.getTransactionParams().do();
+    suggestedParams.fee = algosdk.ALGORAND_MIN_TX_FEE;
+    suggestedParams.flatFee = true;
+    const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: this.config.addr,
+      to,
+      amount: amount * 1000000,
+      suggestedParams,
+    });
+    const completedTxn = await this.signAndSendTxn(txn, this.config.sk);
+    return completedTxn;
   }
 }
